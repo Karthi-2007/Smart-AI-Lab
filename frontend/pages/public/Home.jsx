0f77ee40707181291c1c5f45538406ffea937d9d
Karthi-2007 <Karthikeyanrks2007@gmail.com>
2026-08-06 13:47:54 +0530

    Remove KCECampusSection completely from landing page to prevent displaying blank/broken image blocks

--- frontend/pages/public/Home.jsx
+++ frontend/pages/public/Home.jsx
@@ -5,7 +5,6 @@ import DashboardPreview from "../../components/home/DashboardPreview";
 import Departments from "../../components/home/Departments";
 import Features from "../../components/home/Features";
 import Hero from "../../components/home/Hero";
-import KCECampusSection from "../../components/home/KCECampusSection";
 import Statistics from "../../components/home/Statistics";
 import Testimonials from "../../components/home/Testimonials";
 import Workflow from "../../components/home/Workflow";
@@ -16,14 +15,13 @@ const Home = () => {
       <Hero />
 
       {/* Wave divider */}
-      <div className="-mt-1 overflow-hidden bg-slate-950">
+      <div className="-mt-1 overflow-hidden bg-slate-955" style={{ background: '#f5f7fa' }}>
         <svg viewBox="0 0 1440 60" className="w-full block" preserveAspectRatio="none" style={{ height: "60px", display: "block" }}>
           <path d="M0,0 C360,60 1080,0 1440,60 L1440,0 L0,0 Z" fill="#0f172a" />
         </svg>
       </div>
 
       <Statistics />
-      <KCECampusSection />
       <Features />
       <About />
       <Departments />
