af788eb646ab921c99e91b28f62eebbf1c714bcd
Karthi-2007 <Karthikeyanrks2007@gmail.com>
2026-08-14 11:12:43 +0530

    fix(backend): clean @RequestMapping annotation on SystemSettingController

--- backend/Two Services/smartlab-service/src/main/java/com/smartlab/controller/SystemSettingController.java
+++ backend/Two Services/smartlab-service/src/main/java/com/smartlab/controller/SystemSettingController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping({"/settings", "/api/business/settings"})
+@RequestMapping("/api/business/settings")
 public class SystemSettingController {
 
     @Autowired
